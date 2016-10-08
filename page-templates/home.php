36c1378cc07383e208e420a215dfee38fb1c96e9
Alex Vilchanski <alwizo.com@gmail.com>
2016-10-08 16:54:29 +0200

    Added styles to .features

--- page-templates/home.php
+++ page-templates/home.php
@@ -26,12 +26,14 @@ get_header(); ?>
     </section>
      
     <div  id="content" class="container">
-        
-        <div class="row">
+
+       <div class="row">
         
 	   <div id="primary" class="col-md-12 content-area">
 
-            <main id="main" class="site-main" role="main">
+            <main id="main" class="row site-main" role="main">
+
+                <div class="col-md-12">
 
                 <?php while ( have_posts() ) : the_post(); ?>
 
@@ -39,39 +41,49 @@ get_header(); ?>
 
                  <?php endwhile; // end of the loop. ?>
 
+                </div>
+
             </main><!-- #main -->
             
-            <section class="features">
-                <div class="features__title">
+            <section class="row features">
+                <div class="col-md-12 features__title">
                     Путешествовать с VMP Travel<br> не только приятно, но и выгодно
                 </div>
-                <div class="features__subtitle">
+                <div class="col-md-12 features__subtitle">
                     Швейцария, как она есть. Мы знаем эту страну как самих себя...
                 </div>
-                <div class="row">
-                    <div class="col-md-4 features__item feature">
+                <div class="col-md-4 features__item feature">
+                    <div class="feature__inner">
                         <div class="feature__title">Лучшие Цены</div>
                         <div class="feature__text">
-                            Вы получаете самые выгодные цены на услуги любого сегмента. Это стало возможным благодаря 8-летнему опыту работы на рынке и знанию мира туризма изнутри.
-                        </div>                 
+                            Вы получаете самые выгодные цены на услуги любого сегмента. Это стало возможным благодаря
+                            8-летнему опыту работы на рынке и знанию мира туризма изнутри.
+                        </div>
                     </div>
-                    <div class="col-md-4 features__item feature">
+                </div>
+                <div class="col-md-4 features__item feature">
+                    <div class="feature__inner">
                         <div class="feature__title">Точность и Надежность</div>
                         <div class="feature__text">
-                            Мы работаем точно, как швейцарские часы, чтобы все Ваши заказы выполнялись быстро и качественно. Вы путешествуете и отдыхаете в исторически ценных и живописных уголках Европы совершенно надежно и с гарантией.
-                        </div>                 
+                            Мы работаем точно, как швейцарские часы, чтобы все Ваши заказы выполнялись быстро и
+                            качественно. Вы путешествуете и отдыхаете в исторически ценных и живописных уголках Европы
+                            совершенно надежно и с гарантией.
+                        </div>
                     </div>
-                    <div class="col-md-4 features__item feature">
+                </div>
+                <div class="col-md-4 features__item feature">
+                    <div class="feature__inner">
                         <div class="feature__title">Индивидуальный Подход</div>
                         <div class="feature__text">
-                            Эксклюзивные туры и программы, трансферы, многогранные прогулки и впечатляющие рассказы лучших гидов Европы – это не роскошь. Это то, что Вы точно получаете при бронировании в VMP Travel.
-                        </div>                 
+                            Эксклюзивные туры и программы, трансферы, многогранные прогулки и впечатляющие рассказы
+                            лучших гидов Европы – это не роскошь. Это то, что Вы точно получаете при бронировании в VMP
+                            Travel.
+                        </div>
                     </div>
                 </div>
-                <div class="features__slogan">
-                    Обогащайте свое представление о мире! 
-Удивляйте Ваших близких и бизнес партнеров с нашей помощью!
-                </div>
+            <div class="features__slogan">
+                Обогащайте свое представление о мире!<br>Удивляйте Ваших близких и бизнес партнеров с нашей помощью!
+            </div>
             </section><!-- features -->
            
 	    </div><!-- #primary -->
